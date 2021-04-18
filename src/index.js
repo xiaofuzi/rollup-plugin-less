6c3ce176cede3a51756103d01604d7d94feb9085
vdimir <vdimir@yandex-team.ru>
2021-04-18 11:32:39 +0300

    Add logging

--- src/index.js
+++ src/index.js
@@ -45,14 +45,23 @@ export default function plugin(options = {}) {
             if (options.output && isString(options.output)) {
                 fs.mkdirSync(dirname(options.output), { recursive: true });
 
+                let totalSourcesUsed = 0;
                 let fd = fs.openSync(options.output, 'w')
                 for (let id in generatedData) {
                     if (generatedData[id]['epoch'] < epoch) {
                         continue;
                     }
+                    totalSourcesUsed++;
                     fs.appendFileSync(fd, generatedData[id]['css'])
                 }
                 fs.closeSync(fd)
+
+                if (totalSourcesUsed > 0) {
+                    let totalSourcesUsedMsg = totalSourcesUsed > 1 ? `${totalSourcesUsed} sources` : `one source`;
+                    console.log(`rollup-plugin-less: styles from ${totalSourcesUsedMsg} are written into '${options.output}'`)
+                } else {
+                    console.log(`rollup-plugin-less: no styles`)
+                }
             }
         },
         async transform(code, id) {
@@ -72,6 +81,8 @@ export default function plugin(options = {}) {
             if (options.output && isFunc(options.output)) {
                 css = await options.output(css, id);
             }
+            console.log(`rollup-plugin-less: compile style '${id}'`)
+
             generatedData[id] = {epoch, css}
 
             let exportCode;
